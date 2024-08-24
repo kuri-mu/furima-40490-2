4252d34deb6b8058ae63235b4c60ef09c3da2516
kuri-mu <ajwjpwg0911@gmail.com>
2024-08-24 18:44:28 +0900

    first commit

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,18 @@
 class ApplicationController < ActionController::Base
+  before_action :configure_permitted_parameters, if: :devise_controller?
+  before_action :basic_auth
+
+  private
+
+  def basic_auth
+    authenticate_or_request_with_http_basic do |username, password|
+      username == 'kuri-mu' && password == '0423'
+    end
+  end
+
+  protected
+
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:nickname, :first_name, :last_name, :first_kana, :last_kana, :birth_year, :birth_month, :birth_day,])
+  end
 end
