8208a2a471c42d7a5a001c04a147b76c36b98f85
kuri-mu <ajwjpwg0911@gmail.com>
2024-08-27 16:23:48 +0900

    修正③

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,14 +1,15 @@
 require 'faker'
+
 FactoryBot.define do
   factory :user do
-    nickname              {Faker::Name.initials(number: 2)}
-    email                 {Faker::Internet.email}
-    password              {'1a' + Faker::Internet.password(min_length: 6)}
-    password_confirmation {password}
-    first_name            {Faker::Japanese::Name.first_name}
-    last_name             {Faker::Japanese::Name.last_name}
-    first_kana            {Faker::Japanese::Name.first_name.yomi}
-    last_kana             {Faker::Japanese::Name.last_name.yomi}
-    birth_date            {Faker::Date.birthday(min_age: 18, max_age: 65)}
+    nickname              { Faker::Name.initials(number: 2) }
+    email                 { Faker::Internet.email }
+    password              { '1a' + Faker::Internet.password(min_length: 6) }
+    password_confirmation { password }
+    first_name            { '太郎' }
+    last_name             { '山田' }
+    first_kana            { 'タロウ' }
+    last_kana             { 'ヤマダ' }
+    birth_date            { Faker::Date.birthday(min_age: 18, max_age: 65) }
   end
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -3,319 +3,113 @@ require 'rails_helper'
 RSpec.describe User, type: :model do
   context 'when the user has valid attributes' do
     it 'is valid with valid attributes' do
-      @user = User.new(
-        nickname: 'test',
-        email: 'kkk@gmail.com',
-        password: 'password1',
-        password_confirmation: 'password1',
-        first_name: '山田',
-        last_name: '太郎',
-        first_kana: 'カナ',
-        last_kana: 'カナ',
-        birth_date: '2000-01-01'
-      )
-      expect(@user).to be_valid
+      user = FactoryBot.build(:user)
+      expect(user).to be_valid
     end
   end
 
   context 'when the user has invalid attributes' do
     it 'is invalid without a nickname' do
-      @user = User.new(
-        nickname: '',
-        email: 'kkk@gmail.com',
-        password: 'password1',
-        password_confirmation: 'password1',
-        first_name: '山田',
-        last_name: '太郎',
-        first_kana: 'カナ',
-        last_kana: 'カナ',
-        birth_date: '2000-01-01'
-      )
-      @user.valid?
-      expect(@user.errors.full_messages).to include("Nickname can't be blank")
+      user = FactoryBot.build(:user, nickname: '')
+      user.valid?
+      expect(user.errors.full_messages).to include("Nickname can't be blank")
     end
 
     it 'is invalid without an email' do
-      @user = User.new(
-        nickname: 'test',
-        email: '',
-        password: 'password1',
-        password_confirmation: 'password1',
-        first_name: '山田',
-        last_name: '太郎',
-        first_kana: 'カナ',
-        last_kana: 'カナ',
-        birth_date: '2000-01-01'
-      )
-      @user.valid?
-      expect(@user.errors.full_messages).to include("Email can't be blank")
-    end
-
-
-    it 'is invalid without an email' do
-      @user = User.new(
-        nickname: 'test',
-        email: '',
-        password: 'password1',
-        password_confirmation: 'password1',
-        first_name: '山田',
-        last_name: '太郎',
-        first_kana: 'カナ',
-        last_kana: 'カナ',
-        birth_date: '2000-01-01'
-      )
-      @user.valid?
-      expect(@user.errors.full_messages).to include("Email can't be blank")
+      user = FactoryBot.build(:user, email: '')
+      user.valid?
+      expect(user.errors.full_messages).to include("Email can't be blank")
     end
 
     it 'is invalid without a password' do
-      @user = User.new(
-        nickname: 'test',
-        email: 'kkk@gmail.com',
-        password: '',
-        password_confirmation: 'password1',
-        first_name: '山田',
-        last_name: '太郎',
-        first_kana: 'カナ',
-        last_kana: 'カナ',
-        birth_date: '2000-01-01'
-      )
-      @user.valid?
-      expect(@user.errors.full_messages).to include("Password can't be blank")
+      user = FactoryBot.build(:user, password: '')
+      user.valid?
+      expect(user.errors.full_messages).to include("Password can't be blank")
     end
 
     it 'is invalid when email is in use' do
-      User.create!(
-        nickname: 'test1',
-        email: 'kkk@gmail.com',
-        password: 'password1',
-        password_confirmation: 'password1',
-        first_name: '山田',
-        last_name: '太郎',
-        first_kana: 'カナ',
-        last_kana: 'カナ',
-        birth_date: '2000-01-01'
-      )
-      @user = User.new(
-        nickname: 'test2',
-        email: 'kkk@gmail.com',
-        password: 'password1',
-        password_confirmation: 'password1',
-        first_name: '山田',
-        last_name: '次郎',
-        first_kana: 'カナ',
-        last_kana: 'カナ',
-        birth_date: '2000-02-01'
-      )
-      @user.valid?
-      expect(@user.errors.full_messages).to include('Email has already been taken')
+      FactoryBot.create(:user, email: 'kkk@gmail.com')
+      user = FactoryBot.build(:user, email: 'kkk@gmail.com')
+      user.valid?
+      expect(user.errors.full_messages).to include('Email has already been taken')
     end
 
     it 'is invalid when email does not contain @' do
-      @user = User.new(
-        nickname: 'test',
-        email: 'kkkgmail.com',
-        password: 'password1',
-        password_confirmation: 'password1',
-        first_name: '山田',
-        last_name: '太郎',
-        first_kana: 'カナ',
-        last_kana: 'カナ',
-        birth_date: '2000-01-01'
-      )
-      @user.valid?
-      expect(@user.errors.full_messages).to include('Email is invalid')
+      user = FactoryBot.build(:user, email: 'kkkgmail.com')
+      user.valid?
+      expect(user.errors.full_messages).to include('Email is invalid')
     end
+
     it 'is invalid without a first_name' do
-      @user = User.new(
-        nickname: 'test',
-        email: 'kkk@gmail.com',
-        password: 'password1',
-        password_confirmation: 'password1',
-        first_name: '',
-        last_name: '太郎',
-        first_kana: 'カナ',
-        last_kana: 'カナ',
-        birth_date: '2000-01-01'
-      )
-      @user.valid?
-      expect(@user.errors.full_messages).to include("First name can't be blank")
+      user = FactoryBot.build(:user, first_name: '')
+      user.valid?
+      expect(user.errors.full_messages).to include("First name can't be blank")
     end
 
     it 'is invalid without a last_name' do
-      @user = User.new(
-        nickname: 'test',
-        email: 'kkk@gmail.com',
-        password: 'password1',
-        password_confirmation: 'password1',
-        first_name: '山田',
-        last_name: '',
-        first_kana: 'カナ',
-        last_kana: 'カナ',
-        birth_date: '2000-01-01'
-      )
-      @user.valid?
-      expect(@user.errors.full_messages).to include("Last name can't be blank")
+      user = FactoryBot.build(:user, last_name: '')
+      user.valid?
+      expect(user.errors.full_messages).to include("Last name can't be blank")
     end
 
     it 'is invalid without a first_kana' do
-      @user = User.new(
-        nickname: 'test',
-        email: 'kkk@gmail.com',
-        password: 'password1',
-        password_confirmation: 'password1',
-        first_name: '山田',
-        last_name: '太郎',
-        first_kana: '',
-        last_kana: 'カナ',
-        birth_date: '2000-01-01'
-      )
-      @user.valid?
-      expect(@user.errors.full_messages).to include("First kana can't be blank")
+      user = FactoryBot.build(:user, first_kana: '')
+      user.valid?
+      expect(user.errors.full_messages).to include("First kana can't be blank")
     end
 
     it 'is invalid without a last_kana' do
-      @user = User.new(
-        nickname: 'test',
-        email: 'kkk@gmail.com',
-        password: 'password1',
-        password_confirmation: 'password1',
-        first_name: '山田',
-        last_name: '太郎',
-        first_kana: 'カナ',
-        last_kana: '',
-        birth_date: '2000-01-01'
-      )
-      @user.valid?
-      expect(@user.errors.full_messages).to include("Last kana can't be blank")
+      user = FactoryBot.build(:user, last_kana: '')
+      user.valid?
+      expect(user.errors.full_messages).to include("Last kana can't be blank")
     end
 
     it 'is invalid without a birth_date' do
-      @user = User.new(
-        nickname: 'test',
-        email: 'kkk@gmail.com',
-        password: 'password1',
-        password_confirmation: 'password1',
-        first_name: '山田',
-        last_name: '太郎',
-        first_kana: 'カナ',
-        last_kana: 'カナ',
-        birth_date: ''
-      )
-      @user.valid?
-      expect(@user.errors.full_messages).to include("Birth date can't be blank")
+      user = FactoryBot.build(:user, birth_date: '')
+      user.valid?
+      expect(user.errors.full_messages).to include("Birth date can't be blank")
     end
 
     it 'is invalid with a password containing only numbers' do
-      @user = User.new(
-        nickname: 'test',
-        email: 'kkk@gmail.com',
-        password: '12345678',
-        password_confirmation: '12345678',
-        first_name: '山田',
-        last_name: '太郎',
-        first_kana: 'カナ',
-        last_kana: 'カナ',
-        birth_date: '2000-01-01'
-      )
-      @user.valid?
-      expect(@user.errors.full_messages).to include("Password is invalid, must include both letters and numbers")
+      user = FactoryBot.build(:user, password: '12345678', password_confirmation: '12345678')
+      user.valid?
+      expect(user.errors.full_messages).to include("Password is invalid, must include both letters and numbers")
     end
 
     it 'is invalid with a password containing only letters' do
-      @user = User.new(
-        nickname: 'test',
-        email: 'kkk@gmail.com',
-        password: 'abcdefgh',
-        password_confirmation: 'abcdefgh',
-        first_name: '山田',
-        last_name: '太郎',
-        first_kana: 'カナ',
-        last_kana: 'カナ',
-        birth_date: '2000-01-01'
-      )
-      @user.valid?
-      expect(@user.errors.full_messages).to include("Password is invalid, must include both letters and numbers")
+      user = FactoryBot.build(:user, password: 'abcdefgh', password_confirmation: 'abcdefgh')
+      user.valid?
+      expect(user.errors.full_messages).to include("Password is invalid, must include both letters and numbers")
     end
 
     it 'is invalid with a password containing full-width characters' do
-      @user = User.new(
-        nickname: 'test',
-        email: 'kkk@gmail.com',
-        password: 'あいうえおかきくけこ',
-        password_confirmation: 'あいうえおかきくけこ',
-        first_name: '山田',
-        last_name: '太郎',
-        first_kana: 'カナ',
-        last_kana: 'カナ',
-        birth_date: '2000-01-01'
-      )
-      @user.valid?
-      expect(@user.errors.full_messages).to include("Password is invalid, must include both letters and numbers")
+      user = FactoryBot.build(:user, password: 'あいうえおかきくけこ', password_confirmation: 'あいうえおかきくけこ')
+      user.valid?
+      expect(user.errors.full_messages).to include("Password is invalid, must include both letters and numbers")
     end
 
     it 'is invalid with last_name containing half-width characters' do
-      @user = User.new(
-        nickname: 'test',
-        email: 'kkk@gmail.com',
-        password: 'password1',
-        password_confirmation: 'password1',
-        first_name: '山田',
-        last_name: 'ﾀﾛｳ',
-        first_kana: 'カナ',
-        last_kana: 'カナ',
-        birth_date: '2000-01-01'
-      )
-      @user.valid?
-      expect(@user.errors.full_messages).to include("Last name must be full-width characters")
+      user = FactoryBot.build(:user, last_name: 'ﾀﾛｳ')
+      user.valid?
+      expect(user.errors.full_messages).to include("Last name must be full-width characters")
     end
 
     it 'is invalid with first_name containing half-width characters' do
-      @user = User.new(
-        nickname: 'test',
-        email: 'kkk@gmail.com',
-        password: 'password1',
-        password_confirmation: 'password1',
-        first_name: 'ﾔﾏﾀﾞ',
-        last_name: '太郎',
-        first_kana: 'カナ',
-        last_kana: 'カナ',
-        birth_date: '2000-01-01'
-      )
-      @user.valid?
-      expect(@user.errors.full_messages).to include("First name must be full-width characters")
+      user = FactoryBot.build(:user, first_name: 'ﾔﾏﾀﾞ')
+      user.valid?
+      expect(user.errors.full_messages).to include("First name must be full-width characters")
     end
 
     it 'is invalid with last_kana containing non-Katakana characters' do
-      @user = User.new(
-        nickname: 'test',
-        email: 'kkk@gmail.com',
-        password: 'password1',
-        password_confirmation: 'password1',
-        first_name: '山田',
-        last_name: '太郎',
-        first_kana: 'カナ',
-        last_kana: 'たろう',
-        birth_date: '2000-01-01'
-      )
-      @user.valid?
-      expect(@user.errors.full_messages).to include("Last kana is invalid")
+      user = FactoryBot.build(:user, last_kana: 'たろう')
+      user.valid?
+      expect(user.errors.full_messages).to include("Last kana is invalid")
     end
 
     it 'is invalid with first_kana containing non-Katakana characters' do
-      @user = User.new(
-        nickname: 'test',
-        email: 'kkk@gmail.com',
-        password: 'password1',
-        password_confirmation: 'password1',
-        first_name: '山田',
-        last_name: '太郎',
-        first_kana: 'やまだ',
-        last_kana: 'カナ',
-        birth_date: '2000-01-01'
-      )
-      @user.valid?
-      expect(@user.errors.full_messages).to include("First kana is invalid")
+      user = FactoryBot.build(:user, first_kana: 'やまだ')
+      user.valid?
+      expect(user.errors.full_messages).to include("First kana is invalid")
     end
   end
 end
